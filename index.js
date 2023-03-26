a0721625d93d0711a909afb8178e8c0c13911086
David <davidvo1992@gmail.com>
2023-03-26 18:30:22 -0500

    submit functions continued lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+function saturdayFun(drink = "roller-skate") {
+  return `This Saturday, I want to ${drink}!`;
+}
+saturdayFun("drink alcohol");
+saturdayFun();
+
+function mondayWork(workDay = "go to the office") {
+  return `This Monday, I will ${workDay}.`;
+}
+
+mondayWork("eat chips");
+mondayWork();
+
+function wrapAdjective(defaultResponse = "*") {
+  function innerFunction(adjective = "special") {
+    return `You are ${defaultResponse + adjective + defaultResponse}!`;
+  }
+  return innerFunction;
+}
+wrapAdjective("&")("play games");
